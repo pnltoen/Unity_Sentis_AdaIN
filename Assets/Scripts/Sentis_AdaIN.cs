e870138553c73c3c94fda5edb1085640d58079e7
unknown <pnltoen@gmail.com>
2023-11-17 16:00:53 +0900

    Profile.jpg was deleted and replaced as resources

--- Assets/Scripts/Sentis_AdaIN.cs
+++ Assets/Scripts/Sentis_AdaIN.cs
@@ -9,24 +9,26 @@ using UnityEngine.Profiling;
 public class Sentis_AdaIN : MonoBehaviour
 {
     public ModelAsset _AdaINModel;
-    public Texture2D _style;
     public Texture2D _content;
-    public RenderTexture result;
-    Dictionary<string, Tensor> InputTensors;
+    public Texture2D _style;
+    public RenderTexture _result;
+    Dictionary<string, Tensor> _InputTensors;
 
     //Tensors
     TensorFloat t_style;
     TensorFloat t_content;
 
     //Worker
-    Model m_RuntimeModel;
-    IWorker m_Engine;
+    Model _RuntimeModel;
+    IWorker _Engine;
 
     //UI
+    public GameObject _rawcontent;
+    RawImage _rawcontentImage;
     public GameObject _rawstyle;
-    RawImage rawImageTexture;
+    RawImage _rawstyleImage;
 
-    public enum style_list
+    public enum _Style_List
     {
         antimonocromatismo,
         asheville,
@@ -49,37 +51,42 @@ public class Sentis_AdaIN : MonoBehaviour
         woman_in_peasant_dress_cropped,
         woman_with_hat_matisse
     }
-    public style_list set_style = style_list.antimonocromatismo;
-    Texture2D[] textures;
-    style_list _prev_state;
+    public _Style_List updated_content = _Style_List.antimonocromatismo;
+    public _Style_List updated_style = _Style_List.antimonocromatismo;
+
+    Texture2D[] _textures;
+    _Style_List _prev_style;
+    _Style_List _prev_content;
+
 
     private void Awake()
     {
-        m_RuntimeModel = ModelLoader.Load(_AdaINModel);
-        m_Engine = WorkerFactory.CreateWorker(BackendType.GPUCompute, m_RuntimeModel);
-        rawImageTexture = _rawstyle.GetComponent<RawImage>();
+        _RuntimeModel = ModelLoader.Load(_AdaINModel);
+        _Engine = WorkerFactory.CreateWorker(BackendType.GPUCompute, _RuntimeModel);
+        _rawcontentImage = _rawcontent.GetComponent<RawImage>();
+        _rawstyleImage = _rawstyle.GetComponent<RawImage>();
         LoadResources();
         Sentis_Execute();
     }
     // Start is called before the first frame update
     void Update()
     {
-        Check_Style();
+        Check_Conditions();
     }
 
     void Sentis_Execute()
     {
         //Reset
-        InputTensors = new Dictionary<string, Tensor>();
+        _InputTensors = new Dictionary<string, Tensor>();
         //Resize
-        t_style = TextureConverter.ToTensor(textures[(int)set_style], 512, 512, 3); //이미지 크기 조절
-        t_content = TextureConverter.ToTensor(_content);
+        t_style = TextureConverter.ToTensor(_textures[(int)updated_style], 512, 512, 3);
+        t_content = TextureConverter.ToTensor(_textures[(int)updated_content], 512, 512, 3);
         //Add
-        InputTensors.Add("style", t_style);
-        InputTensors.Add("content", t_content);
+        _InputTensors.Add("style", t_style);
+        _InputTensors.Add("content", t_content);
         //Output
-        TensorFloat t_output = m_Engine.Execute(InputTensors).PeekOutput() as TensorFloat;
-        TextureConverter.RenderToTexture(t_output, result);
+        TensorFloat t_output = _Engine.Execute(_InputTensors).PeekOutput() as TensorFloat;
+        TextureConverter.RenderToTexture(t_output, _result);
         //Dispose
         t_style.Dispose();
         t_content.Dispose();
@@ -88,20 +95,29 @@ public class Sentis_AdaIN : MonoBehaviour
 
     void LoadResources()
     {
-        int enumLength = Enum.GetValues(typeof(style_list)).Length;
-        textures = new Texture2D[enumLength];
+        int enumLength = Enum.GetValues(typeof(_Style_List)).Length;
+        _textures = new Texture2D[enumLength];
         for (int i = 0; i < enumLength; i++)
         {
-            string _name = ((style_list)i).ToString();
-            textures[i] = Resources.Load<Texture2D>(_name);
+            string _name = ((_Style_List)i).ToString();
+            _textures[i] = Resources.Load<Texture2D>(_name);
         }
     }
 
-    void Check_Style()
+    void Check_Conditions()
     {
-        if (set_style == _prev_state) { }
-        _prev_state = set_style;
-        rawImageTexture.texture = textures[(int)set_style];
-        Sentis_Execute();
+        if (updated_style != _prev_style)
+        {
+            _prev_style = updated_style;
+            _rawstyleImage.texture = _textures[(int)updated_style];
+            Sentis_Execute();
+        }
+
+        if (updated_content != _prev_content)
+        {
+            _prev_content = updated_content;
+            _rawcontentImage.texture = _textures[(int)updated_content];
+            Sentis_Execute();
+        }
     }
 }
